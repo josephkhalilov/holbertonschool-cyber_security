f37bb92c7b1680801297bac7fa64cd78488468ad
josephkhalilov <stormgainucun1@gmail.com>
2025-12-26 10:33:03 -0500

    2connections

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
-echo "Rules updated"
-echo "Rules updated (v6)"
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT && echo "Rules updated"
+sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT && echo "Rules updated (v6)"
