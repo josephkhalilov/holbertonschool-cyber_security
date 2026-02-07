89dd9c44761b815687bcd9029cdcef5e572b375c
josephkhalilov <stormgainucun1@gmail.com>
2026-02-07 04:50:39 -0500

    crypto

--- cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
+++ cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-# A script that hashes a password using SHA-1 and saves it to a file
-
-# Echo the first argument ($1) without a trailing newline
-# Pipe it into sha1sum to generate the hash
-# Use awk to print only the hash (removing the trailing "  -")
-# Redirect the output to 0_hash.txt
 echo -n "$1" | sha1sum | awk '{print $1}' > 0_hash.txt
