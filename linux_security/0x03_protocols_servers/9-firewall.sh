9408468c7d1cf54ad1f209858953a884ebfb7bae
josephkhalilov <stormgainucun1@gmail.com>
2026-02-14 13:07:25 -0500

    Protocols

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 iptables -P INPUT DROP
-iptables -A INPUT -p tcp --dport ssh -j ACCEPT; iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p tcp --dport ssh -j ACCEPT; iptables -A INPUT -i lo -j DROP
