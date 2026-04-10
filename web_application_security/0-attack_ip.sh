e033d7d8f9463e7ea94eed966d053af4ba6e18db
josephkhalilov <stormgainucun1@gmail.com>
2026-04-10 06:13:25 -0400

    incident response

---
+++ web_application_security/0-attack_ip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+LOG_FILE="logs.txt"
+
+if [ ! -f "$LOG_FILE" ]; then
+    echo "Xəta: $LOG_FILE tapılmadı!"
+    exit 1
+fi
+
+awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
