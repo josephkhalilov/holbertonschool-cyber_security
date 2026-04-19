256a5245c87e9a4b22c15c5661cc8b1350610662
josephkhalilov <stormgainucun1@gmail.com>
2026-04-19 10:06:35 -0400

    web forensics

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-# 3-ips.sh: Uğurlu giriş edən 18 unikal IP-ni tapan skript
-
-LOG_FILE="auth.log"
-
-if [ ! -f "$LOG_FILE" ]; then
-    exit 1
-fi
-
-# 1. "Accepted password" olan sətirləri seçirik (bu, uğurlu qırılmaları göstərir)
-# 2. "from" sözündən dərhal sonra gələn IP ünvanını kəsib götürürük
-# 3. Unikal ünvanları sıralayırıq
-# 4. Sayırıq
-grep "Accepted password" "$LOG_FILE" | awk '{for(i=1;i<=NF;i++) if($i=="from") print $(i+1)}' | sort -u | wc -l
+grep "Accepted password for root" auth.log | awk '{print$(NF-3)}' | sort | uniq -c | wc -l
