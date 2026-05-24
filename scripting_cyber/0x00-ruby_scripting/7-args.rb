c52a8ea07e5110b29c25dec7de94ff29f36ac772
josephkhalilov <stormgainucun1@gmail.com>
2026-05-24 15:57:37 -0400

    ruby scripting

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,13 +1,11 @@
 def print_arguments
+  # Check if the global ARGV array is empty
   if ARGV.empty?
     puts "No arguments provided."
   else
-    # Print the header expected by the sandbox checker
-    puts "Arguments:"
-    
-    # Print each argument on a new line
-    ARGV.each do |arg|
-      puts arg
+    # Loop through arguments with their index, starting the count at 1
+    ARGV.each_with_index do |arg, index|
+      puts "#{index + 1}. #{arg}"
     end
   end
 end
