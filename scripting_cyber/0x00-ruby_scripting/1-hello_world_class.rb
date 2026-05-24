a2a981e9efefae5bd99b8dbb3071d1e472957a4b
josephkhalilov <stormgainucun1@gmail.com>
2026-05-24 15:40:10 -0400

    ruby scripting

--- scripting_cyber/0x00-ruby_scripting/1-hello_world_class.rb
+++ scripting_cyber/0x00-ruby_scripting/1-hello_world_class.rb
@@ -1,19 +1,11 @@
-Write a Ruby script that prints "Hello, World!" using a class.
+class HelloWorld
+  # Initialize method sets the instance variable '@message'
+  def initialize
+    @message = "Hello, World!"
+  end
 
-    Class HelloWorld
-        Use method that sets an instance
-            create variable called message that hold the string Hello, World!
-        Create a method that display the message called print_hello
-
-┌──(imen㉿hbtn-lab)-[…/scripting_cyber/0x00-ruby_scripting]
-└─$ cat 1-main.rb 
-require_relative '1-hello_world_class'
-
-# Create an instance of HelloWorld, change the message, and call the print_hello method
-hello_world_instance = HelloWorld.new
-
-hello_world_instance.print_hello
-
-┌──(imen㉿hbtn-lab)-[…/scripting_cyber/0x00-ruby_scripting]
-└─$ ruby 1-main.rb 
-Hello, World!
+  # Method to display the @message variable
+  def print_hello
+    puts @message
+  end
+end
